ec670aced14018afbf9081da45608af9c7a99292
Jürgen Kartnaller <juergen@kartnaller.at>
2016-07-13 17:20:33 +0200

    full document data structure

--- etc/sql/sql.sql
+++ etc/sql/sql.sql
@@ -7,6 +7,26 @@ CREATE TABLE petitions (
         expires TIMESTAMP
     ),
     state STRING,
+    tags ARRAY(STRING),
+    title STRING,
+    city STRING,
+    type STRING,
+    description STRING INDEX OFF,
+    suggested_solution STRING INDEX OFF,
+
+    images ARRAY(STRING),
+    links ARRAY(STRING),
+    videos ARRAY(STRING),
+    connected_locations ARRAY(STRING),
+
+    signatures OBJECT(STRICT) AS (
+        amount LONG,
+        required LONG
+    ),
+
+    owner STRING,
+    response_token STRING,
+
     db_class__ STRING INDEX OFF
 )
 CLUSTERED INTO 5 SHARDS
--- iris/service/petition/document.py
+++ iris/service/petition/document.py
@@ -1,3 +1,5 @@
+import copy
+
 from lovely.esdb.document import Document
 from lovely.esdb.properties import Property
 from lovely.essequence import Sequence
@@ -25,6 +27,78 @@ class Petition(Document):
           The current state of the petition.
           The state is controlled via a state machine.
         """
+    )
+
+    tags = Property(
+        default=lambda: copy.copy([]),
+        doc="""
+          A list of tags which are searchable.
+        """,
+    )
+
+    title = Property(
+        default=''
+    )
+
+    city = Property(
+        default=None,
+        doc="""
+          A reference to a city (id).
+        """
+    )
+
+    type = Property(
+        default='',
+        doc="""
+          A string defined by the frontend (no internal meaning).
+        """
+    )
+
+    description = Property(
+        default=''
+    )
+
+    suggested_solution = Property(
+        default=''
+    )
+
+    images = Property(
+        default=[],
+        doc="A list of file ids."
+    )
+
+    links = Property(
+        default=lambda: copy.copy([]),
+        doc="A list of location ids."
+    )
+
+    videos = Property(
+        default=lambda: copy.copy([]),
+        doc="A list of location ids to youtube videos."
+    )
+
+    connected_locations = Property(
+        default=lambda: copy.copy([]),
+        doc="""
+          A list of location ids to location which are connected to this
+          petition.
+        """
+    )
+
+    signatures = Property(
+        default=lambda: copy.copy({}),
+        doc="""
+          An object which contains information about the signatures of the
+          petition.
+        """
+    )
+
+    owner = Property(
+        default=None
+    )
+
+    response_token = Property(
+        default=None
     )
 
     def __repr__(self):
