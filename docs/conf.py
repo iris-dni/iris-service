e70ce6e0585d025f4148b2f649295b1c4615b1d1
Bernd Rössl <bernd.roessl@gmail.com>
2016-07-13 16:37:19 +0200

    running plantuml headless

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ extensions = ['sphinxcontrib.plantuml',
               'sphinxcontrib.httpdomain',
               'sphinx.ext.doctest']
 
-plantuml = 'java -jar parts/plantuml/plantuml.jar -graphvizdot /opt/local/bin/dot'
+plantuml = 'java -Djava.awt.headless=true -jar parts/plantuml/plantuml.jar -graphvizdot /opt/local/bin/dot'  # noqa
 
 # load doctest extension to be able to setup testdata in the documentation that
 # is hidden in the generated html (by using .. doctest:: :hide:)
