a60d2926d5923856e186809b6ed007d69c140ca8
Jürgen Kartnaller <juergen@kartnaller.at>
2016-09-19 09:05:09 +0200

    made og data swagger data generic

--- iris/service/og/service.py
+++ iris/service/og/service.py
@@ -4,6 +4,8 @@ from pyramid import security
 
 from lovely.pyrest.rest import RestService, rpcmethod_route, rpcmethod_view
 
+from iris.service.rest.swagger import swagger_reduce_response
+
 from ..endpoint import EndpointErrorMixin
 
 from .og import og_data_for_url
@@ -18,6 +20,7 @@ class OGCheckServiceAPI(EndpointErrorMixin):
     @rpcmethod_route(request_method='POST')
     @rpcmethod_view(http_cache=0,
                     permission=security.NO_PERMISSION_REQUIRED)
+    @swagger_reduce_response
     def check(self, url):
         try:
             data = og_data_for_url(url)
