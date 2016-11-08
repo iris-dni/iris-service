3799533c89c35257f25933367be3e15aee2d12e5
Jürgen Kartnaller <juergen@kartnaller.at>
2016-11-08 15:15:37 +0100

    simplify acl

--- iris/service/content/petition/acl.py
+++ iris/service/content/petition/acl.py
@@ -8,7 +8,6 @@ class PublicPetitionServiceAuthFactory(BaseAuthFactory):
     """Allows the admin role the get access to the full API
     """
 
-    def __acl__(self):
-        return [
-            (security.Allow, acl.Roles.Admin, acl.Permissions.AdminPetition)
-        ]
+    __acl__ = [
+        (security.Allow, acl.Roles.Admin, acl.Permissions.AdminPetition)
+    ]
